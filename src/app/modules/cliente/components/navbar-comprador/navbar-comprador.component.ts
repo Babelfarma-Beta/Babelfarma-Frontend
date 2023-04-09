79471dab3be0973f8c5aedf1f05fa118229aa94f
Angel Sosa <gabrielo.09.angel@gmail.com>
2023-04-09 02:15:20 -0500

    asdasd

--- src/app/modules/cliente/components/navbar-comprador/navbar-comprador.component.ts
+++ src/app/modules/cliente/components/navbar-comprador/navbar-comprador.component.ts
@@ -1,47 +1,41 @@
-
 import { Router, ActivatedRoute } from '@angular/router';
 import { CarritoDeComprasService } from '../../../../services/carrito-de-compras.service';
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-comprador',
   templateUrl: './navbar-comprador.component.html',
   styleUrls: ['./navbar-comprador.component.css']
 })
-
-export class NavbarCompradorComponent implements OnDestroy {
-
-  idComprador!:any;
-
+export class NavbarCompradorComponent implements OnInit, OnDestroy {
+  idComprador: any;
   mobileQuery!: MediaQueryList;
-
   fillerNav = Array.from({ length: 5 }, (_, i) => `Nav Item ${i + 1}`);
-
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private media: MediaMatcher,
     private carritoService: CarritoDeComprasService,
     private router: Router,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
-    this.loadId();
+  }
+
+  ngOnInit(): void {
+    this.idComprador = this.route.snapshot.params['id'];
   }
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
-  loadId(){
-    this.idComprador = this.route.snapshot.params['id'];
-  }
-
-  salir(){
+  salir(): void {
     this.carritoService.setproductosCarrito([]);
     this.router.navigate(['/']);
   }
-
-
 }
