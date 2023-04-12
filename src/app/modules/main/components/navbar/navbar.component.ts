8ace7907b44292dea4926917a684bb3365b434a5
Angel Sosa <gabrielo.09.angel@gmail.com>
2023-04-12 17:45:23 -0500

    navbar de inicio

--- src/app/modules/main/components/navbar/navbar.component.ts
+++ src/app/modules/main/components/navbar/navbar.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CarritoDeComprasService } from '../../../../services/carrito-de-compras.service';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-  showMenu = false;
-  constructor() { }
+export class NavbarComponent implements OnInit, OnDestroy {
+
+  mobileQuery!: MediaQueryList;
+  fillerNav = Array.from({ length: 5 }, (_, i) => `Nav Item ${i + 1}`);
+  private _mobileQueryListener: () => void;
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private media: MediaMatcher,
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 500px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+   }
 
   ngOnInit(): void {
     const buttons = document.querySelectorAll('.button');
@@ -19,9 +34,8 @@ export class NavbarComponent implements OnInit {
     });
   }
 
-
-  toggleMenu() {
-    this.showMenu = !this.showMenu;
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
 }
